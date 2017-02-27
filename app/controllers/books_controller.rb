25c032ce7dd7d314b9f8718ea8c47637c5b52f82
Dan Lin <dlin2102@gmail.com>
2017-02-27 00:33:25 -0500

    Book CRUD

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,49 @@
 class BooksController < ApplicationController
 
-def index
-#  @book = Books.all
+  def index
+    @books = Book.all.order('title DESC')
+  end
+
+def show
+   @book = Book.find(params[:id])
 end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.create!(book_params)
+
+    if @book.save
+      redirect_to book_path(@book)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :author, :description)
+  end
 
 end
