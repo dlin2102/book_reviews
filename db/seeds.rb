eae576ef2903f5d97edcda6a914e97f3b62d7028
Dan Lin <dlin2102@gmail.com>
2017-03-01 17:04:37 -0500

    added some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Category.destroy_all
+Book.destroy_all
+Review.destroy_all
+
+users = User.create([
+  {email:"clin2102@yahoo.com", password:"danlin77"},
+  {email: "dlin2102@gmail.com", password:"danlin77"}
+])
+
+categories = Category.create([
+  {name:"Romance"},
+  {name:"Children's Books"},
+  {name:"Young Adult"},
+  {name:"Fiction"},
+  {name:"Fantasy"},
+  {name:"Triller"},
+  {name:"Cookbooks"},
+  {name:"Art"},
+  {name:"Technology"},
+  {name:"Biographies"},
+  {name:"History"}
+  ])
